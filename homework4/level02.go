15973309ba393cf4f93ad4072e554c033612d3cc
iGxnon <1545766400@qq.com>
2021-10-31 13:46:12 +0800

    stuff

--- homework4/level02.go
+++ homework4/level02.go
@@ -1,17 +1,23 @@
 package main
 
-import (
-	"os"
-)
-
-func main() {
-	file, err := os.Create("./plan.txt")
-	if err != nil {
-		return
-	}
-	_, err2 := file.Write([]byte("I’m not afraid of difficulties and insist on learning programming"))
-	if err2 != nil {
-		return
-	}
-
-}
+//func main() {
+//	file, err := os.Create("./plan.txt")
+//	if err != nil {
+//		return
+//	}
+//	_, err2 := file.Write([]byte("I’m not afraid of difficulties and insist on learning programming"))
+//	if err2 != nil {
+//		return
+//	}
+//
+//	file, _ = os.Open("./plan.txt")
+//	r := make([]byte, 100) // 简单读取
+//	read, err3 := file.Read(r)
+//	if err3 != nil {
+//		if err3 == io.EOF {
+//			fmt.Println(err3)
+//		}
+//		return
+//	}
+//	fmt.Println(string(r[:read]))
+//}
---
+++ homework4/level03.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 9个缓存区，避免没必要的阻塞
+	over := make(chan bool, 9)
+	for i := 0; i < 10; i++ {
+		go func(i int) {
+			fmt.Println(i)
+			over <- true
+		}(i)
+		// 应该在最后一个goroutine里往channel里写入true
+		// 或者往goroutine里写入九次，并在主协程里读取九次
+		//if i == 9 {
+			//over <- true
+		//}
+	}
+	// 接收9次，少了最后一次的话就阻塞
+	for i := 0; i < 10; i++ {
+		<-over
+	}
+	fmt.Println("over!!!")
+}
