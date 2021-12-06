fe446de27b583c73122849653e53f4ac5a5672f7
iGxnon <1545766400@qq.com>
2021-12-06 23:48:27 +0800

    stuff

--- homework7And8/cmd/main.go
+++ homework7And8/cmd/main.go
@@ -1,20 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"homework7And8/api"
+	"homework7And8/dao"
+)
 
 func main() {
-	//dao.InitializeDefault()
-	//api.RegisterRouter()
-	fmt.Println("CREATE TABLE `tree_hollow` (" +
-		"`id` BIGINT(20) NOT NULL AUTO_INCREMENT," +
-		"`prefix` VARCHAR(20) DEFAULT ''," +
-		"`model_json` MEDIUMTEXT NOT NULL," +
-		"`model_serialized_img` MEDIUMTEXT NOT NULL," +
-		"`loc` NOT NULL TINYTEXT," +
-		"`form_title` NOT NULL VARCHAR(20) DEFAULT ''," +
-		"`form_texts` NOT NULL TINYTEXT," +
-		"`animation` TINYTEXT," +
-		"`particle` TINYTEXT," +
-		"PRIMARY KEY(`id`, `prefix`)" + // 	双主键
-		")ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;")
+	dao.InitializeDefault()
+	api.RegisterRouter()
 }
--- homework7And8/service/comment.go
+++ homework7And8/service/comment.go
@@ -1,1 +1,10 @@
 package service
+
+import (
+	"homework7And8/dao"
+	"homework7And8/model"
+)
+
+func AddComment(comment model.Comment) error {
+	return dao.InsertComment(comment)
+}
