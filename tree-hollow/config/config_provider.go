a5e0c19c36235cf7170848cee2d50f6b58ea2299
iGxnon <1545766400@qq.com>
2021-12-10 00:22:25 +0800

    docs(config): add format of config.json

--- tree-hollow/config/config_provider.go
+++ tree-hollow/config/config_provider.go
@@ -15,6 +15,19 @@ type Configuration struct {
 	JWTTimeOut       int64  `json:"jwt_time_out"`
 }
 
+/*
+config.json 模板
+{
+  "default_db_name": "tree_hollows_db",
+  "default_ip_and_port": "localhost:3306",
+  "default_root": "root",
+  "default_password": "******",
+  "default_charset": "utf8mb4",
+  "jwt_key": "伟神嘿嘿我的伟神嘿嘿",
+  "jwt_time_out": 172800
+}
+*/
+
 var Config Configuration
 
 func InitializeConfiguration() {
