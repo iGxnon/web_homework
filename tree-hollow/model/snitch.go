1470eda43c0472a535dbff888e39ed4077768dba
iGxnon <1545766400@qq.com>
2021-12-09 10:19:02 +0800

    feat(api): initialize api

--- tree-hollow/model/snitch.go
+++
@@ -1,7 +0,0 @@
-package model
-
-type Snitch struct {
-	Id       int    `json:"id"`
-	Name     string `json:"name"`
-	Password string `json:"password"`
-}
--- tree-hollow/utils/algorithm.go
+++
@@ -1,8 +0,0 @@
-package utils
-
-import "homework7And8/model"
-
-// BFS 对评论图进行广搜
-func BFS(rootCommentId int, rawComments []model.Comment) []model.Comment {
-	return nil
-}
