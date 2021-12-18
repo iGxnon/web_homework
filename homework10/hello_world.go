3de899133e59d9a4dd4ed7491f327f50e06d30e3
iGxnon <1545766400@qq.com>
2021-12-18 18:18:53 +0800

    update

--- homework10/hello_world.go
+++ homework10/hello_world.go
@@ -1,1 +1,14 @@
-package homework10
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	engine := gin.Default()
+	engine.Any("/hi", func(context *gin.Context) {
+		context.JSON(200, "Hello World!")
+	})
+	err := engine.Run(":80")
+	if err != nil {
+		return
+	}
+}
